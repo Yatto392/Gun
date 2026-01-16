c8fc62ef80f0ab688969f0d548e599f91403f543
Yatto392 <6rvdbrfhvy@privaterelay.appleid.com>
2026-01-16 19:01:41 +0100

    キャラ操作と背景を追加

---
+++ Assets/Script/Kyaracon.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kyaracon : MonoBehaviour
+{
+    public List<GameObject> targetObjects;
+    private int currentIndex = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Vector3 newPosition = transform.position;
+                newPosition.x = targetObjects[currentIndex].transform.position.x;
+                transform.position = newPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetObjects == null || targetObjects.Count == 0)
+        {
+            return;
+        }
+
+        // Aキーで前のオブジェクトへ移動 (インデックス減少)
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                Vector3 newPosition = transform.position;
+                newPosition.x = targetObjects[currentIndex].transform.position.x;
+                transform.position = newPosition;
+            }
+        }
+
+        // Dキーで次のオブジェクトへ移動 (インデックス増加)
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (currentIndex < targetObjects.Count - 1)
+            {
+                currentIndex++;
+                Vector3 newPosition = transform.position;
+                newPosition.x = targetObjects[currentIndex].transform.position.x;
+                transform.position = newPosition;  
+            }
+        }
+    }
+}
