fd3ec1f490867c3b7639e45ed546fee0d6ff8c6d
Yatto392 <6rvdbrfhvy@privaterelay.appleid.com>
2026-01-24 19:58:16 +0100

    アプデ

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,13 +4,6 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
-    [Header("Vision")]
-    public float viewRadius = 15f;
-    [Range(0, 360)]
-    public float viewAngle = 90f;
-    public LayerMask targetMask;
-    public LayerMask obstacleMask;
-
     [Header("Shooting")]
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
@@ -19,7 +12,6 @@ public class Enemy : MonoBehaviour
     public float rotationSpeed = 5f;
 
     private Transform player;
-    private bool isPlayerVisible = false;
 
     void Start()
     {
@@ -32,47 +24,17 @@ public class Enemy : MonoBehaviour
         {
             Debug.LogError("Player not found! Make sure the player has the 'Player' tag.");
         }
-
-        StartCoroutine(FindTargetsWithDelay(0.2f));
     }
 
     void Update()
     {
-        if (isPlayerVisible)
-        {
-            if (Time.time > lastFireTime + fireCooldown)
-            {
-                Fire();
-            }
-        }
-    }
-
-    IEnumerator FindTargetsWithDelay(float delay)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(delay);
-            FindVisibleTargets();
-        }
-    }
-
-    void FindVisibleTargets()
-    {
-        isPlayerVisible = false;
         if (player == null) return;
 
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        FacePlayer();
 
-        if (distanceToPlayer < viewRadius)
+        if (Time.time > lastFireTime + fireCooldown)
         {
-            Vector3 dirToPlayer = (player.position - transform.position).normalized;
-            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
-            {
-                if (!Physics.Raycast(transform.position, dirToPlayer, distanceToPlayer, obstacleMask))
-                {
-                    isPlayerVisible = true;
-                }
-            }
+            Fire();
         }
     }
 
@@ -96,34 +58,5 @@ public class Enemy : MonoBehaviour
         Quaternion bulletLookRotation = Quaternion.LookRotation(directionToPlayer);
 
         Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletLookRotation);
-    }
-
-    // For debugging in the editor
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, viewRadius);
-
-        Vector3 viewAngleA = DirFromAngle(-viewAngle / 2, false);
-        Vector3 viewAngleB = DirFromAngle(viewAngle / 2, false);
-
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position + viewAngleA * viewRadius);
-        Gizmos.DrawLine(transform.position, transform.position + viewAngleB * viewRadius);
-
-        if (isPlayerVisible)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(transform.position, player.position);
-        }
-    }
-
-    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
-    {
-        if (!angleIsGlobal)
-        {
-            angleInDegrees += transform.eulerAngles.y;
-        }
-        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
