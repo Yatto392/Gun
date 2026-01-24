8d36ed7040a267c0373e9c7e9bf26cd7bebf9a9d
Yatto392 <6rvdbrfhvy@privaterelay.appleid.com>
2026-01-24 10:46:20 +0100

    アプデ

--- Assets/Script/BillBoard.cs
+++ Assets/Script/BillBoard.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class BillBoard : MonoBehaviour 
 {
+	public Vector3 rotationOffset;
 
 	void Update () 
     {
 		Vector3 p = Camera.main.transform.position;
 		p.y = transform.position.y;
 		transform.LookAt (p);
+		transform.rotation *= Quaternion.Euler(rotationOffset);
 	}
 }
